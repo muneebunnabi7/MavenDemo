2d2f7f7ebe23871124286ae03aa43c9f18f244de
muneebunnabi7 <muneebunnabi582@gmail.com>
2018-11-16 13:18:08 +0530

    testing push

---
+++ src/main/java/com/controller/MavenServlet.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MavenServlet
+ */
+public class MavenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MavenServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String myname =  request.getParameter("name");
+		PrintWriter out= response.getWriter();
+		out.println(myname);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ target/FirstMaven/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="MavenServlet">
+  <input type="text" name ="name"></input>
+  <input type="button" value="submit" ></input>
+</form>
+</body>
+</html>
